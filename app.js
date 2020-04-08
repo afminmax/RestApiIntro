d6361354ecdabe3d5dfb4fdf5cd563d85975d713
afminmax <allen.frasier@aquinix.com>
2020-04-08 12:07:27 +0200

    changed to chain routing

--- app.js
+++ app.js
@@ -21,45 +21,83 @@ const articleSchema = new mongoose.Schema({
 const Article = mongoose.model('Article', articleSchema);
 // ------------------------- MONGODB DECLARATIONS ------------------------------------ //
 
-//HTTP Verb "GET" - will fetch all the articles
-app.get('/articles', function (req, res) {
-  Article.find({}, function (err, foundArticles) {
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
-  });
-});
+// //HTTP Verb "GET" - will fetch all the articles
+// app.get('/articles', function (req, res) {
+//   Article.find({}, function (err, foundArticles) {
+//     if (!err) {
+//       res.send(foundArticles);
+//     } else {
+//       res.send(err);
+//     }
+//   });
+// });
 
-//HTTP Verb "POST" - will add another article to the database
-app.post('/articles', function (req, res) {
-  console.log(req.body.title);
-  console.log(req.body.content);
+// //HTTP Verb "POST" - will add another article to the database
+// app.post('/articles', function (req, res) {
+//   console.log(req.body.title);
+//   console.log(req.body.content);
 
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  newArticle.save(function (err) {
-    if (!err) {
-      res.send('successful article load.');
-    } else {
-      res.send(err);
-    }
-  });
-});
+//   const newArticle = new Article({
+//     title: req.body.title,
+//     content: req.body.content,
+//   });
+//   newArticle.save(function (err) {
+//     if (!err) {
+//       res.send('successful article load.');
+//     } else {
+//       res.send(err);
+//     }
+//   });
+// });
 
-//HTTP Verb "DELETE" - will fetch all the articles
-app.delete('/articles', function (req, res) {
-  Article.deleteMany(function (err) {
-    if (!err) {
-      res.send('successfully deleted all articles');
-    } else {
-      res.send(err);
-    }
+// //HTTP Verb "DELETE" - will fetch all the articles
+// app.delete('/articles', function (req, res) {
+//   Article.deleteMany(function (err) {
+//     if (!err) {
+//       res.send('successfully deleted all articles');
+//     } else {
+//       res.send(err);
+//     }
+//   });
+// });
+
+// ExpressJS Route Chaining
+app
+  .route('/articles')
+  .get(function (req, res) {
+    Article.find({}, function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function (req, res) {
+    console.log(req.body.title);
+    console.log(req.body.content);
+
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send('successful article load.');
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send('successfully deleted all articles');
+      } else {
+        res.send(err);
+      }
+    });
   });
-});
 
 app.listen(3000, function () {
   console.log('...server has started on port 3000');
